92a086f5f0bf3d340ad78a71dc76489d154718f9
DagmaraSobczak <daga.sobczak@gmail.com>
2023-06-02 21:55:02 +0200

    contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const { name } = this.state;
+    if (name.trim() === '') {
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name: name,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+      name: '',
+    }));
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+    return (
+      <>
+        <ContactForm
+          name={name}
+          onFormSubmit={this.handleFormSubmit}
+        />
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import css from './ContactForm/ContactForm.module.css';
+
+//import { nanoid } from 'nanoid';
+
+export default class ContactForm extends Component {
+  render() {
+    return (
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    );
+  }
+}
